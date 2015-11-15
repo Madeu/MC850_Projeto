4055ba3f5c14b57d734c996c78f347324c7dc3c4
Amadeu <madeu.bonfante@gmail.com>
2015-11-15 19:18:37 -0200

    Cluster function defined

--- demo/clusteringPlate.c
+++ demo/clusteringPlate.c
@@ -1,19 +1,13 @@
-#include "ift.h"
-
-#define EPSILON 0.0001
-#define MAX_INTERATION 1000
+#include "iftSeedsGenerator.h"
 
 int main() {
-	
-	iftDataSet* dataSet = iftReadXYDataSet("points");
+	iftImage* img = iftReadImageByExt("lena.pgm");
+	iftVoxel u[50];
+	int size = readPoint("points.txt", u);
 
-	iftDataSet* centers = iftKmeansInitCentroidsFromSamples(dataSet, 2);
+	iftDataSet* centers = generateCentroids(u, size);
 
-	iftKmeansRun(0, dataSet, &centers, MAX_INTERATION, EPSILON);
-
-	iftDraw2DFeatureSpace(centers, 0, 0);
-
-	iftDestroyDataSet(&dataSet);
+	iftWriteImageP2(img, "nova_lena.pgm");
 	iftDestroyDataSet(&centers);
 
 	return 0;
